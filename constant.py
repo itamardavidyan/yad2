4039bd0d0e5435e7edd5b0114f54c3697612762b
itamardavidyan <itamardavidyan@gmail.com>
2018-10-03 19:28:51 +0300

    vscode + constant

---
+++ constant.py
@@ -0,0 +1,4 @@
+CHROMELOCATION = "C:\\Users\\MKM\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"
+# CHROMELOCATION = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+CHROMEDRIVER = "C:\\Users\\MKM\\Documents\\GitHub\\yad2\\drivers\\chromedriver.exe"
+# CHROMEDRIVER = 'C:\\python\\yad2\drivers\\chromedriver.exe'
--- yad2auto.py
+++ yad2auto.py
@@ -2,57 +2,63 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import constant
+
 # from selenium.webdriver.chrome.options import Options
 
 options = webdriver.ChromeOptions()
-options.binary_location = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-options.add_argument('--headless')
-options.add_argument('window-size=1200x600')
-driver = webdriver.Chrome(executable_path=r'C:\\python\\yad2\drivers\\chromedriver.exe', chrome_options=options)
+options.binary_location = constant.CHROMELOCATION
+options.add_argument("--headless")
+options.add_argument("window-size=1200x600")
+driver = webdriver.Chrome(executable_path=constant.CHROMEDRIVER, chrome_options=options)
 
-driver.get('http://www.yad2.co.il/')
+driver.get("http://www.yad2.co.il/")
 driver.implicitly_wait(5)
-driver.execute_script("window.scrollTo(0, 200)") 
+driver.execute_script("window.scrollTo(0, 200)")
 driver.implicitly_wait(5)
 # take screenshot
-driver.get_screenshot_as_file('main-page.png')
+driver.get_screenshot_as_file("main-page.png")
 
 # enter email
-element = driver.find_element_by_css_selector('#login_email')
-element.send_keys('itamardavidyan@gmail.com')
+element = driver.find_element_by_css_selector("#login_email")
+element.send_keys("itamardavidyan@gmail.com")
 
 # enter password
-element = driver.find_element_by_xpath("//*[@id=\"mockpass\"]/td/input")
+element = driver.find_element_by_xpath('//*[@id="mockpass"]/td/input')
 element.click()
-element = driver.find_element_by_xpath("//*[@id=\"Irealpass\"]")
-element.send_keys('Yad2Pass')
+element = driver.find_element_by_xpath('//*[@id="Irealpass"]')
+element.send_keys("Yad2Pass")
 
 # click login button
-element = driver.find_element_by_xpath("//*[@id=\"mainIndex\"]/div[2]/div[4]/form/div/table/tbody/tr[4]/td/input")
+element = driver.find_element_by_xpath(
+    '//*[@id="mainIndex"]/div[2]/div[4]/form/div/table/tbody/tr[4]/td/input'
+)
 element.click()
 
 # take screenshot
-driver.get_screenshot_as_file('login-page.png')
+driver.get_screenshot_as_file("login-page.png")
 
 # move to AD
-driver.get('https://my.yad2.co.il/newOrder/index.php?action=personalAreaFeed&CatID=3&SubCatID=0')
+driver.get(
+    "https://my.yad2.co.il/newOrder/index.php?action=personalAreaFeed&CatID=3&SubCatID=0"
+)
 
 # take screenshot
-driver.get_screenshot_as_file('ad-page.png')
+driver.get_screenshot_as_file("ad-page.png")
 
 # open ad
-element = driver.find_element_by_xpath("//*[@id=\"feed\"]/tbody/tr[2]/td[9]/div")
+element = driver.find_element_by_xpath('//*[@id="feed"]/tbody/tr[2]/td[9]/div')
 element.click()
 
 # take screenshot
-driver.get_screenshot_as_file('after-open-ad.png')
+driver.get_screenshot_as_file("after-open-ad.png")
 
-# update ad
-driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
-driver.switch_to.default_content()
-element = driver.find_element_by_xpath("//*[@id=\"order_form\"]/div[2]/div[1]/div[2]/div[2]/div/div/div[2]/div")
-element.click()
+# # update ad
+# driver.switch_to.frame(driver.find_element_by_tag_name("iframe"))
+# driver.switch_to.default_content()
+# element = driver.find_element_by_xpath("//*[@id=\"order_form\"]/div[2]/div[1]/div[2]/div[2]/div/div/div[2]/div")
+# element.click()
 
-# take screenshot
-driver.get_screenshot_as_file('after-updtae-page.png')
+# # take screenshot
+# driver.get_screenshot_as_file('after-updtae-page.png')
 
